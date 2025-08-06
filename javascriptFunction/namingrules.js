7e39ed75a21d7af4ce30c09b6e8ee1cfb36d8d35
msaheers <mishalsaheersha10@gmail.com>
2025-08-06 12:17:41 +0100

    foundation Javascript

---
+++ javascriptFunction/namingrules.js
@@ -0,0 +1,18 @@
+// Use small letters, but every new word startsd with a capital letter. 
+// example: calcularAreaRetangulo, criarPessoa
+
+// Try to use verbs in function names: 
+
+// * sayHello
+// * getName
+// *calculateAge
+
+// Scope (where variables live)
+
+// Think of scope like rooms in a house
+// * A variable inside a room (function) cant be seen from outside. 
+// If you have two boxes with the same name in different rooms, it's fine. 
+
+function test() {
+    let x = 5; // only exists here
+}
--- javascriptFunction/practice3.js
+++ javascriptFunction/practice3.js
@@ -1,3 +1,9 @@
 // Parameters vs Arguments 
 // Parameter = the name used in the function (like name above).
 // Argument = the actual value you pass (like "John").
+
+function add(a, b) {
+    return a + b; 
+}
+
+add(5, 3); // 5 and 3 are arguments
